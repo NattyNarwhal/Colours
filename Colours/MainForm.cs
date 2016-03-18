18737804496cd4b8cc09894805494f85c51a12ec
Calvin <calvin@openmailbox.org>
2016-03-18 17:10:00 -0300

    oops, fix format

--- Colours/MainForm.cs
+++ Colours/MainForm.cs
@@ -69,7 +69,7 @@ namespace Colours
                 tableLayoutPanel1.ColumnStyles[i].Width = 100 / lc.Count;
 
                 ColorButton cb = new ColorButton(next.ToRgb());
-                cb.Text = String.Format("R {0}\r\nG {0}\r\nB {0}",
+                cb.Text = String.Format("R {0}\r\nG {1}\r\nB {2}",
                     cb.Color.R, cb.Color.G, cb.Color.B);
                 toolTip1.SetToolTip(cb, String.Format("{0}\r\n{1}\r\n{2}",
                     ColorTranslator.ToHtml(cb.Color), cb.Color.ToRgbString(), cb.Color.ToHslString()));
