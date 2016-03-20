bc1fe82ec76bb039e617a876554b6707a74c47db
Calvin <calvin@openmailbox.org>
2016-03-20 12:13:11 -0300

    improve GUI parity
    configuration and clipboard are next

--- Colours.Gtk/MainWindow.cs
+++ Colours.Gtk/MainWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using Gtk;
 using Colours;
 
@@ -30,15 +31,9 @@ public partial class MainWindow: Gtk.Window
 		mainVbox.Remove (newBox);
 
 		newBox = new HBox (true, 6);
-
-		int i = 0;
 		foreach (HsvColor c in app.Results) {
 			ColorButton cb = new ColorButton (c.ToGdkColor ());
-			if (i++ == 0) {
-				cb.ColorSet += OnColorChooserColorChanged;
-			} else {
-				cb.Sensitive = false;
-			}
+			cb.ColorSet += OnColorChooserColorChanged;
 			cb.TooltipText = String.Format("{0}\r\n{1}\r\n{2}\r\n{3}",
 				System.Drawing.ColorTranslator.ToHtml(c.ToRgb()),
 				c.ToRgb().ToRgbString(),
@@ -46,6 +41,13 @@ public partial class MainWindow: Gtk.Window
 				c.ToString());
 			newBox.PackStart(cb, true, true, 0);
 		}
+
+		undoAction.Sensitive = app.CanUndo ();
+		redoAction.Sensitive = app.CanRedo ();
+		BrightenAction.Sensitive = app.CanBrighten ();
+		DarkenAction.Sensitive = app.CanDarken ();
+		SaturateAction.Sensitive = app.CanSaturate ();
+		DesaturateAction.Sensitive = app.CanDesaturate ();
 
 		mainVbox.Add (newBox);
 		this.ShowAll ();
@@ -68,6 +70,77 @@ public partial class MainWindow: Gtk.Window
 	{
 		app.SchemeType = GetComboBoxSelection ();
 		app.GetSchemeResults ();
+		SyncAppViewState ();
+	}
+
+	protected void OnUndoActionActivated (object sender, EventArgs e)
+	{
+		app.Undo();
+		SyncAppViewState();
+	}	protected void OnInvertActionActivated (object sender, EventArgs e)
+	{
+		app.SetColor (app.Color.Invert (), true);
+		SyncAppViewState ();
+	}
+
+	protected void OnDesaturateActionActivated (object sender, EventArgs e)
+	{
+		app.Desaturate ();
+		SyncAppViewState ();
+	}
+
+	protected void OnSaturateActionActivated (object sender, EventArgs e)
+	{
+		app.Saturate ();
+		SyncAppViewState ();
+	}
+	protected void OnDarkenActionActivated (object sender, EventArgs e)
+	{
+		app.Darken ();
+		SyncAppViewState ();
+	}
+
+	protected void OnBrightenActionActivated (object sender, EventArgs e)
+	{
+		app.Brighten ();
+		SyncAppViewState ();
+	}
+
+	protected void OnRandomActionActivated (object sender, EventArgs e)
+	{
+		Random r = new Random ();
+		app.SetColor (Color.FromArgb (r.Next (255), r.Next (255), r.Next (255)), true);
+		SyncAppViewState ();
+	}
+
+	protected void OnPasteActionActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnCopyHSVActionActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}	
+
+	protected void OnCopyHSLActionActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnCopyRGBActionActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnCopyHexActionActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnRedoActionActivated (object sender, EventArgs e)
+	{
+		app.Redo ();
 		SyncAppViewState ();
 	}
 }
