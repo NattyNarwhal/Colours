2ef14b40da1de7748b096e97071c518d97082053
Calvin <calvin@openmailbox.org>
2016-03-20 11:45:28 -0300

    GTK version stub
    let's hope MonoDevelop is capable of not mangling SLN files

---
+++ Colours.Gtk/GdkWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace Colours
+{
+	public static class GdkWrapper
+	{
+		public static Color ToGDIColor(this Gdk.Color c)
+		{
+			// GDK colour channels are ushorts, not bytes
+			// except the constructor DOES take bytes
+			return Color.FromArgb (c.Red >> 8, c.Green >> 8, c.Blue >> 8);
+		}
+
+		public static HsvColor ToHsvColor(this Gdk.Color c)
+		{
+			return new HsvColor(ToGDIColor(c));
+		}
+
+		public static Gdk.Color ToGdkColor(this Color c)
+		{
+			return new Gdk.Color(c.R, c.G, c.B);
+		}
+
+		public static Gdk.Color ToGdkColor(this HsvColor c)
+		{
+			return ToGdkColor(c.ToRgb());
+		}
+	}
+}
+
---
+++ Colours.Gtk/MainWindow.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Gtk;
+using Colours;
+
+public partial class MainWindow: Gtk.Window
+{
+	public AppController app;
+	HBox newBox;
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		app = new AppController (new HsvColor (System.Drawing.Color.Red), GetComboBoxSelection());
+		SyncAppViewState ();
+	}
+
+	public string GetComboBoxSelection()
+	{
+		TreeIter iter;
+
+		if (schemeBox.GetActiveIter (out iter))
+			return (string)schemeBox.Model.GetValue (iter, 0);
+		else
+			throw new Exception ();
+	}
+
+	public void SyncAppViewState()
+	{
+		mainVbox.Remove (newBox);
+
+		newBox = new HBox (true, 6);
+
+		int i = 0;
+		foreach (HsvColor c in app.Results) {
+			ColorButton cb = new ColorButton (c.ToGdkColor ());
+			if (i++ == 0) {
+				cb.ColorSet += OnColorChooserColorChanged;
+			} else {
+				cb.Sensitive = false;
+			}
+			cb.TooltipText = String.Format("{0}\r\n{1}\r\n{2}\r\n{3}",
+				System.Drawing.ColorTranslator.ToHtml(c.ToRgb()),
+				c.ToRgb().ToRgbString(),
+				c.ToRgb().ToHslString(),
+				c.ToString());
+			newBox.PackStart(cb, true, true, 0);
+		}
+
+		mainVbox.Add (newBox);
+		this.ShowAll ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnColorChooserColorChanged (object sender, EventArgs e)
+	{
+		ColorButton cb = (ColorButton)sender;
+		app.SetColor (cb.Color.ToGDIColor (), true);
+		SyncAppViewState ();
+	}
+
+	protected void OnSchemeBoxChanged (object sender, EventArgs e)
+	{
+		app.SchemeType = GetComboBoxSelection ();
+		app.GetSchemeResults ();
+		SyncAppViewState ();
+	}
+}
