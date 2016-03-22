b5d5f20a548e7762fbbb93761b37d97774b4fd28
Calvin <calvin@openmailbox.org>
2016-03-22 11:15:42 -0300

    HTML proof generation and wireup to the SWF version
    this way, you can also delegate printing/etc to the HTML page. fancy formatting can be added down the road, possibly even multiple schemes at  the same time.

---
+++ Colours.Core/HtmlProofGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Colours
+{
+    public static class HtmlProofGenerator
+    {
+        const string page = "<!doctype html><html>"
+            + "<head><title>{0}</title> <meta name=\"generator\""
+            + "content=\"https://github.com/NattyNarwhal/Colours\" /></head>"
+            + "<body>{1}</body></html>";
+
+        const string table = "<table><tr>"
+            + "<td rowspan=\"4\" style=\"width: 1em; background-color: {0}\"><td>{0}</td></tr>"
+            + "<tr><td>{1}</td></tr>"
+            + "<tr><td>{2}</td></tr>"
+            + "<tr><td>{3}</td></tr></table>";
+
+        public static string GenerateTable(List<HsvColor> colors)
+        {
+            return String.Join("", colors.Select(c =>
+                String.Format(table,
+                    ColorTranslator.ToHtml(c.ToRgb()),
+                    c.ToRgb().ToRgbString(),
+                    c.ToRgb().ToHslString(),
+                    c.ToString()
+                    )
+                )
+            );
+        }
+
+        public static string GeneratePage(string title, string body)
+        {
+            return String.Format(page, title, body);
+        }
+    }
+}
