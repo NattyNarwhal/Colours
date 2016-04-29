a85728f831ee86fe3a5a72a3a27fe07ab5d4f1b2
Calvin <calvin@openmailbox.org>
2016-04-29 15:42:26 -0300

    use regexp to handle comments and columns in palettes

--- Colours.Core/Models/Palette.cs
+++ Colours.Core/Models/Palette.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Colours
 {
@@ -13,6 +14,10 @@ namespace Colours
     public class Palette
     {
         const string magic = "GIMP Palette";
+
+        const string nameRegex = "Name: ?(.*)";
+        const string columnsRegex = @"Columns: ?(\d*)";
+        const string commentRegex = @"#\s*(.*)";
 
         /// <summary>
         /// Gets or sets the name of the palette.
@@ -89,20 +94,20 @@ namespace Colours
             foreach (string l in file)
             {
                 if (l == magic || l == "#") continue;
-                if (l.StartsWith("# "))
+                if (Regex.IsMatch(l, commentRegex))
                 {
-                    // TODO: handle comments without space
-                    Comments.Add(l.Remove(0, 2));
+                    Comments.Add(Regex.Match(l, commentRegex).Groups[1].Value);
                 }
-                else if (l.StartsWith("Columns: "))
+                else if (Regex.IsMatch(l, columnsRegex))
                 {
                     int i = 0; // a default
-                    int.TryParse(l.Remove(0, "Columns: ".Length), out i);
+                    int.TryParse(Regex.Match(l,
+                        columnsRegex).Groups[1].Value, out i);
                     Columns = i;
                 }
-                else if (l.StartsWith("Name: "))
+                else if (Regex.IsMatch(l, nameRegex))
                 {
-                    Name = l.Remove(0, "Name: ".Length);
+                    Name = Regex.Match(l, nameRegex).Groups[1].Value;
                 }
                 else
                 {
