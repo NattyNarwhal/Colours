68a75b4823a592ebb2ac99af5f515e2709b67c84
Calvin <calvin@openmailbox.org>
2016-05-09 09:11:16 -0300

    as an experiment to learn F#, a console frontend to the colour schemeing component
    this is a command-line program, for one-shot usage, like many a good unix tool.

---
+++ Colours.Console.Mixer/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Colours
+
+let hsvHtml (hsv: HsvColor) = hsv.ToRgb().ToHtml();
+
+[<EntryPoint>]
+let main argv = 
+    let parsed =
+        AppArgParser.ParseArgs(argv, new HsvColor(0.0, 1.0, 1.0), SchemeType.Complement)
+
+    let results =
+        match parsed.SchemeType with
+            | SchemeType.Analogous -> ColorSchemer.Analogous(parsed.Color)
+            | SchemeType.Complement -> ColorSchemer.Complement(parsed.Color)
+            | SchemeType.Monochromatic -> ColorSchemer.Monochromatic(parsed.Color)
+            | SchemeType.SplitComplements -> ColorSchemer.SplitComplement(parsed.Color)
+            | SchemeType.Triads -> ColorSchemer.Triads(parsed.Color)
+            | SchemeType.Tetrads -> ColorSchemer.Tetrads(parsed.Color)
+            | _ -> System.Collections.Generic.List<HsvColor>() // this seems clunky
+
+    for hsv in results do
+        let html = hsvHtml hsv
+        printfn "%s" html
+
+    0 // return an integer exit code
