139542f65e5c335ce560035e57591885580646ad
Calvin <calvin@openmailbox.org>
2016-05-09 09:33:36 -0300

    use the AppController to handle it

--- Colours.Console.Mixer/Program.fs
+++ Colours.Console.Mixer/Program.fs
@@ -10,21 +10,14 @@ let trueColorEscape (s: string, r, g, b) =
 let main argv = 
     let parsed =
         AppArgParser.ParseArgs(argv, new HsvColor(0.0, 1.0, 1.0), SchemeType.Complement)
+    let controller = new AppController(parsed)
 
     let visual =
         if not (isNull(parsed.UnparsedArgs)) then
             parsed.UnparsedArgs.Contains("-v")
         else false
 
-    let results =
-        match parsed.SchemeType with
-            | SchemeType.Analogous -> ColorSchemer.Analogous(parsed.Color)
-            | SchemeType.Complement -> ColorSchemer.Complement(parsed.Color)
-            | SchemeType.Monochromatic -> ColorSchemer.Monochromatic(parsed.Color)
-            | SchemeType.SplitComplements -> ColorSchemer.SplitComplement(parsed.Color)
-            | SchemeType.Triads -> ColorSchemer.Triads(parsed.Color)
-            | SchemeType.Tetrads -> ColorSchemer.Tetrads(parsed.Color)
-            | _ -> System.Collections.Generic.List<HsvColor>() // this seems clunky
+    let results = controller.Results
 
     for hsv in results do
         let rgb = hsv.ToRgb();
