b26e6321d8df5e794a5d564408c3f1d924b3c6d3
Calvin <calvin@openmailbox.org>
2016-10-17 18:11:15 -0300

    GTK Sort dialog

---
+++ Colours.Gtk/SortDialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using global::Gtk;
+
+namespace Colours
+{
+	public partial class SortDialog : Dialog
+	{
+		public SortDialog()
+		{
+			this.Build();
+
+			sortByBox.Model = new ListStore(typeof(PaletteSortBy));
+			foreach (var sb in Enum.GetValues(typeof(PaletteSortBy)))
+				sortByBox.Model.AppendValues(sb);
+
+			var sortRender = new CellRendererText();
+			sortByBox.PackStart(sortRender, true);
+			sortByBox.SetCellDataFunc(sortRender, new CellLayoutDataFunc((cl, c, m, i) =>
+			{
+				((CellRendererText)c).Text = ((PaletteSortBy)sortByBox.Model.GetValue(i, 0)).ToString();
+			}));
+		}
+
+		public PaletteSortBy SortBy => Enum.GetValues(typeof(PaletteSortBy))[sortByBox.Active];
+
+		public bool Ascending => orderBox.Active;
+	}
+}
---
+++ Colours.Gtk/gtk-gui/Colours.SortDialog.cs
@@ -0,0 +1,14 @@
+
+namespace Colours
+{
+	public partial class SortDialog
+	{
+		private Gtk.Button buttonCancel;
+
+		private Gtk.Button buttonOk;
+
+		private void Build()
+		{
+		}
+	}
+}
