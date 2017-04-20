f76689d2eadf0cde4efeec25c86ebbcf240adc0d
Calvin <calvin@openmailbox.org>
2017-04-20 19:36:42 -0300

    Actually get DPI awareness (very basic)
    Not the best, but don't feel ready to force 4.7 yet, and a lot of the methods like manifests don't seem to work - you call this instead. FWIW, linking WPF in seems to also call this, but we don't need to do that...
    
    FIXME: Add a manifest so we can look for Windows 8.1, not 8.0.

--- Colours/Program.cs
+++ Colours/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,6 +10,9 @@ namespace Colours
 {
     static class Program
     {
+        [DllImport("shcore.dll")]
+        static extern int SetProcessDpiAwareness(int value);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -18,6 +22,9 @@ namespace Colours
             var state = AppArgParser.ParseArgs(args,
                 Properties.Settings.Default.LastColor.ToRgbColor().ToHsv(),
                 Properties.Settings.Default.SchemeType);
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT &&
+                Environment.OSVersion.Version == new Version(6,2))
+                SetProcessDpiAwareness(1);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm(state));
